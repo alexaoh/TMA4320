49d4590c7b9fe57d27af50100331d4ea667bc4ee
alexaoh <alex.andunge@gmail.com>
2020-03-08 16:11:21 +0100

    Continued working on Project2, almost done

--- Project2/loader.py
+++ Project2/loader.py
@@ -39,8 +39,8 @@ def get_file_names(dataset, path):
         fname_img = os.path.join(path, 'train-images-idx3-ubyte')
         fname_lbl = os.path.join(path, 'train-labels-idx1-ubyte')
     elif dataset == "testing":
-        fname_img = os.path.join(path, 't10k-images-idx3-ubyte')
-        fname_lbl = os.path.join(path, 't10k-labels-idx1-ubyte')
+        fname_img = os.path.join(path, 't10k-images.idx3-ubyte')
+        fname_lbl = os.path.join(path, 't10k-labels.idx1-ubyte')
     else:
         raise ValueError("dataset must be 'testing' or 'training'")
     return fname_img, fname_lbl
